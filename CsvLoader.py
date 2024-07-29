4b994e109d6495ac0a65ba78825e2c0b4e6e1e8e
jayiuk <jayiuk987@gmail.com>
2024-07-29 14:09:17 +0900

    create csvloader class

--- CsvLoader.py
+++ CsvLoader.py
@@ -2,7 +2,11 @@ import langchain
 import langchain_core
 import langchain_text_splitters
 from langchain_community.document_loaders.csv_loader import CSVLoader
+import os
 
+path = './consumer_data.csv'
+encoding = 'utf-8'
+source_column = '고객번호'
 
 class csvload():
     def __init__(self, path, encoding, source_column):
