cbdd16aa1368d64b2cb43667fb083768f492557c
jayiuk <jayiuk987@gmail.com>
2024-07-30 15:51:25 +0900

    embedding method

--- DocLoader.py
+++ DocLoader.py
@@ -6,6 +6,11 @@ import os
 from langchain_community.document_loaders import JSONLoader
 from langchain_text_splitters import RecursiveJsonSplitter
 import json
+from dotenv import load_dotenv
+load_dotenv()
+from langchain.embeddings import HuggingFaceEmbeddings
+
+model_name = "intfloat/multilingual-e5-large-instruct"
 
 path = './consumer_data.csv'
 encoding = 'utf-8'
@@ -23,17 +28,20 @@ class docload():
         data = loader.load()
         return data
     
-    def get_json(self, path, schema, text_content):
-        loader = JSONLoader(file_path = path, jq_schema = schema, text_content = text_content)
+    def get_json(j_path, schema, text_content):
+        loader = JSONLoader(file_path = j_path, jq_schema = schema, text_content = text_content)
         j_data = loader.load()
         return j_data
     
-    def json_split(max_chunk_size, js_path):
-        with open(js_path, 'r') as f:
-            js_data = json.load(f)
+    def json_split(max_chunk_size, js_data):
         splitter = RecursiveJsonSplitter(max_chunk_size = max_chunk_size)
         json_chunk = splitter.split_json(json_data = js_data)
         return json_chunk
+    
+    def embedding(model_name, model_kwarg, encode_k, chunked_data):
+        hf_embedding = HuggingFaceEmbeddings(model_name = model_name, model_kwargs = model_kwarg, encode_kwargs = encode_k)
+        get_embed = hf_embedding.embed_documents(chunked_data)
+        return get_embed
 
 c = docload()
 d = c.get_csv(path, encoding, source_column)
