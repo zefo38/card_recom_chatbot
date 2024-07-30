b45c80d859216bb5a4ab771079ed5021cd9ef66c
jayiuk <jayiuk987@gmail.com>
2024-07-30 23:20:15 +0900

    checking DocLoader

--- DocLoader.py
+++ DocLoader.py
@@ -3,7 +3,7 @@ import langchain_core
 import langchain_text_splitters
 from langchain_community.document_loaders.csv_loader import CSVLoader
 import os
-from langchain_community.document_loaders import JSONLoader
+from langchain_community.document_loaders import DirectoryLoader
 from langchain_text_splitters import RecursiveJsonSplitter
 import json
 from dotenv import load_dotenv
@@ -16,12 +16,12 @@ model_name = "intfloat/multilingual-e5-large-instruct"
 path = './consumer_data.csv'
 encoding = 'utf-8'
 source_column = '고객번호'
-j_path = './045/01-1/Training/labeled'
+d_path = './source2'
 
 class docload():
     def __init__(self, path, j_path, embedding_model_name):
         self.path = path
-        self.j_path = j_path
+        self.d_path = d_path
         self.embedding_model_name = embedding_model_name
 
     def get_csv(self, path, encoding, source_column):
@@ -29,11 +29,11 @@ class docload():
         data = loader.load()
         return data
     
-    def get_json(self, j_path, schema, text_content, max_chunk_size):
-        loader = JSONLoader(file_path = self.j_path, jq_schema = schema, text_content = text_content)
-        j_data = loader.load()
+    def get_dir(self, d_path, glob, loader_cls, silent_errors = True, max_chunk_size):
+        loader = DirectoryLoader(file_path = self.d_path, glob = glob, loader_cls = loader_cls, silent_errors = True)
+        d_data = loader.load()
         splitter = RecursiveJsonSplitter(max_chunk_size = max_chunk_size)
-        json_chunk = splitter.split_json(json_data = j_data)
+        json_chunk = splitter.split_json(json_data = d_data)
         return json_chunk
     
     def embedding(self, embedding_model_name, model_kwarg, encode_k, chunked_data):
@@ -41,6 +41,6 @@ class docload():
         get_embed = hf_embedding.embed_documents(chunked_data)
         return get_embed
 
-c = docload(path, j_path, model_name)
-d = c.get_json(, )
+c = docload(path, d_path, model_name)
+d = c.get_dir()
 print(d)
