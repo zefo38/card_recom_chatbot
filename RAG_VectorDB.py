fbd6605f61852ed8825c60534e6b72047858e9cc
jayiuk <jayiuk987@gmail.com>
2024-08-01 14:45:38 +0900

    making vectordb class

--- RAG_VectorDB.py
+++ RAG_VectorDB.py
@@ -2,10 +2,8 @@ from DocLoader import docload
 from langchain_community.vectorstores import FAISS
 
 class vectordb(docload):
-    def __init__(self, embedding_model_name, data):
-        super().__init__(self, embedding_model_name)
-        self.data = data
+    def __init__(self, embedding_model_name, model_kwargs, encode_kwargs):
+        super().__init__(self, embedding_model_name, model_kwargs, encode_kwargs)
         
-    def save_embed(self):
-        super().embedding()
-        
+    def save_embed(self, embedding_model_name, model_kwargs, encode_kwargs):
+        embed = docload.embedding(self.embedding_model_name, self.model_kwargs, self.encode_kwargs)
