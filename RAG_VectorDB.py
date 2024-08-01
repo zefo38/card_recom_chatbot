89af939a020e80e2bed9135d6a23299d8d6ca645
jayiuk <jayiuk987@gmail.com>
2024-08-01 14:15:33 +0900

    creating vectordb

--- RAG_VectorDB.py
+++ RAG_VectorDB.py
@@ -1,1 +1,11 @@
 from DocLoader import docload
+from langchain_community.vectorstores import FAISS
+
+class vectordb(docload):
+    def __init__(self, embedding_model_name, data):
+        super().__init__(self, embedding_model_name)
+        self.data = data
+        
+    def save_embed(self):
+        super().embedding()
+        
