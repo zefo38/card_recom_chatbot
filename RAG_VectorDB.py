e4fb10f64d55f4b7e90eac5075dc3b8230ceefa0
jayiuk <jayiuk987@gmail.com>
2024-08-02 10:46:06 +0900

    making vectordb class

--- RAG_VectorDB.py
+++ RAG_VectorDB.py
@@ -1,21 +1,27 @@
 from DocLoader import docload
 from langchain_community.vectorstores import FAISS
+from langchain.retrievers.ensemble import EnsembleRetriever
 
-faiss_db = FAISS.from_documents()
 
-class vectordb(docload):
-    def __init__(self, embedding_model_name, model_kwargs, encode_kwargs, input_data, db, query):
-        super().__init__(self, embedding_model_name)
+class vectordb():
+    def __init__(self, embedding_model_name, model_kwargs, encode_kwargs, input_data):
+        self.embedding_model_name = embedding_model_name
         self.model_kwargs = model_kwargs
         self.encode_kwargs = encode_kwargs
         self.input_data = input_data
-        self.db = db
-        self.query = query
+
         
-    def save_embed(self, db):
-        embed = docload.embedding(self.embedding_model_name, self.model_kwargs, self.encode_kwargs, self.input_data)
-        db = self.db(self.input_data, embed)
+    def init_db(self, embedding, distance_strategy):
+        db = FAISS.from_documents(self.input_data, embedding = embedding, distance_strategy = distance_strategy)
         return db
     
-    def querying(self, query):
-        
+    def db_save(self, db, path):
+        return db.save_local(folder_path=path)
+    
+    def db_load(self, path, embedding):
+        loaded_db = FAISS.load_local(path, embedding)
+        return loaded_db
+    
+    def ensemble_ret(self, rets, weights, c):
+        ret = EnsembleRetriever(retrievers = rets, weights = weights, c = c)
+        return ret
