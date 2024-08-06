84da246c89352c95806f708d94b641929b6ededa
jayiuk <jayiuk987@gmail.com>
2024-08-06 16:42:19 +0900

    checking

--- chain_test.py
+++ chain_test.py
@@ -22,13 +22,14 @@ from langchain_ollama.llms import OllamaLLM
 from transformers import AutoModel
 from langchain_core.output_parsers import StrOutputParser
 from operator import itemgetter
+import pandas as pd
 
-llm = OllamaLLM(model = 'llama3:8b')
+llm = OllamaLLM(model = 'llama3:8b', temperature = 0.0)
 
 data = './processed_file(1) (9).csv'
 
 distance_strategy = DistanceStrategy.COSINE
-memory = ConversationBufferMemory(memory_key = 'chat_history')
+memory = ConversationBufferMemory(memory_key = 'chat_history', return_messages = True)
 store = {}
 session_ids = 'test1'
 embedding_model_name = "intfloat/multilingual-e5-large"
@@ -60,7 +61,12 @@ ensemble = vec.ensemble_ret([basic_ret, bm25], [0.5, 0.5], 2)
 chain = chat_chain(llm, memory, ensemble)
 
 account_chain = chain.get_chain_account(data_path = data)
-result = account_chain.predict(query = "고객 번호가 1번인 사람이 미용에 총 얼마 썼는지 알려줘")
+result = account_chain.predict(query = "내가 카페에 총 얼마 썼는지 알려줘")
 print(result)
-result2 = account_chain.predict(query = "그 사람이 서점엔 얼마나 썼는지 알려줘")
+context = chain.save_memory("내가 카페에 총 얼마 썼는지 알려줘", result)
+result2 = account_chain.predict(query = "그럼 서점엔 얼마나 썼는지 알려줘")
 print(result2)
+context = chain.save_memory("그럼 서점엔 얼마나 썼는지 알려줘", result2)
+result3 = account_chain.predict(query = "그 둘의 총합을 알려줘")
+context = chain.save_memory("그 둘의 총합을 알려줘", result3)
+print(result3)
