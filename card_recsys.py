be3caad290e346014c1b53fe5d391361c603da76
jayiuk <jayiuk987@gmail.com>
2024-08-07 10:25:42 +0900

    making card recsys module

---
+++ card_recsys.py
@@ -0,0 +1,24 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from sklearn.neighbors import KernelDensity
+import pacmap
+
+class recsys():
+    def __init__(self, data, random_state):
+            self.data = data
+            self.random_state = random_state
+
+    def preprocessing(self, n_components, n_neighbors, MN_ratio, FP_ratio):
+          scaler = StandardScaler()
+          scaled_data = scaler(self.data)
+          embedding = pacmap.PaCMAP(n_components = n_components, n_neighbors = n_neighbors, MN_ratio = MN_ratio, FP_ratio = FP_ratio)
+          pac_data = embedding.fit_transform(scaled_data)
+          return pac_data
+    
+    def clustering(self, n_cluster, pre_data):
+          km = KMeans(n_clusters = n_cluster)
+          clusters = km.fit_transform(pre_data)
+          return clusters
+    
+    def get_sum_score(self, )
--- chain_test.py
+++ chain_test.py
@@ -34,7 +34,9 @@ store = {}
 session_ids = 'test1'
 embedding_model_name = "intfloat/multilingual-e5-large"
 d_path = './source4'
+d1_path = './consume_data'
 v_path = './test_db'
+v2_path = './test_db2'
 distance_strategy = DistanceStrategy.COSINE
 
 embedding = HuggingFaceEmbeddings(model_name = embedding_model_name, model_kwargs = {'device' : 'cpu'}, encode_kwargs = {"normalize_embeddings" : True})
@@ -42,32 +44,39 @@ embedding = HuggingFaceEmbeddings(model_name = embedding_model_name, model_kwarg
 
 
 c = docload(d_path, embedding_model_name)
+c1 = docload(d1_path, embedding_model_name)
 d = c.get_dir(glob = '**/*.tsv', loader_cls = CSVLoader, silent_errors = False, loader_kwargs = {'autodetect_encoding':True})
-data = c.get_dir(glob = '**/*.csv', loader_cls = CSVLoader, silent_errors = False, loader_kwargs = {'autodetect_encoding':True})
+data = c1.get_dir(glob = '**/*.csv', loader_cls = CSVLoader, silent_errors = False, loader_kwargs = {'autodetect_encoding':True})
 t = c.split_text(d, chunk_size = 200, chunk_overlap = 50)
+print(data)
 
 
 
 vec = vectordb(embedding, d)
+vec2 = vectordb(embedding, data)
 db = vec.init_db(distance_strategy=distance_strategy)
+db3 = vec2.init_db(distance_strategy = distance_strategy)
+db3 = vec.db_save(v2_path, db3)
 db = vec.db_save(v_path, db)
 db2 = vec.db_load(path = v_path)
+db4 = vec.db_load(path = v2_path)
 
-basic_ret = vec.db_ret(db2, 2)
-bm25 = vec.bm_ret(d, 2)
+basic_ret = vec.db_ret(db4, 2)
+bm25 = vec.bm_ret(data, 2)
 ensemble = vec.ensemble_ret([basic_ret, bm25], [0.5, 0.5], 2)
+docs = ensemble.invoke("고객번호 1번인 사람이 카페에 총 얼마나 썼어?")
+print(docs)
 
 
+#chain = chat_chain(llm, memory, ensemble)
 
-chain = chat_chain(llm, memory, ensemble)
-
-account_chain = chain.get_chain_account(data_path = data)
-result = account_chain.predict(query = "내가 카페에 총 얼마 썼는지 알려줘")
-print(result)
-context = chain.save_memory("내가 카페에 총 얼마 썼는지 알려줘", result)
-result2 = account_chain.predict(query = "그럼 서점엔 얼마나 썼는지 알려줘")
-print(result2)
-context = chain.save_memory("그럼 서점엔 얼마나 썼는지 알려줘", result2)
-result3 = account_chain.predict(query = "그 둘의 총합을 알려줘")
-context = chain.save_memory("그 둘의 총합을 알려줘", result3)
-print(result3)
+#account_chain = chain.get_chain_account(data_path = data)
+#result = account_chain.predict(query = "내가 카페에 총 얼마 썼는지 알려줘")
+#print(result)
+#context = chain.save_memory("내가 카페에 총 얼마 썼는지 알려줘", result)
+#result2 = account_chain.predict(query = "그럼 서점엔 얼마나 썼는지 알려줘")
+#print(result2)
+#context = chain.save_memory("그럼 서점엔 얼마나 썼는지 알려줘", result2)
+#result3 = account_chain.predict(query = "그 둘의 총합을 알려줘")
+#context = chain.save_memory("그 둘의 총합을 알려줘", result3)
+#print(result3)
