d68e5f6faf209be3fab2650a62b779fb860f1089
jayiuk <jayiuk987@gmail.com>
2024-08-09 16:10:49 +0900

    pdf loader

--- DocLoader.py
+++ DocLoader.py
@@ -13,6 +13,7 @@ from sentence_transformers import SentenceTransformer
 from datasets import Dataset
 from multiprocess import Pool
 from langchain_core.documents.base import Document
+from langchain_community.document_loaders import PyPDFDirectoryLoader
 
 
 embedding_model_name = "intfloat/multilingual-e5-large"
@@ -32,7 +33,12 @@ class docload():
         loader = DirectoryLoader(self.d_path, glob = glob, loader_cls = loader_cls, silent_errors = silent_errors, loader_kwargs = loader_kwargs)
         d_data = loader.load()
         return d_data
-        
+    
+    def pdf_dir(self, glob, silent_errors):
+        loader = PyPDFDirectoryLoader(self.d_path, glob = glob, silent_errors = silent_errors)
+        pdf_data = loader.load()
+        return pdf_data
+
     def split_text(self, t_data, chunk_size, chunk_overlap):
         spliter = RecursiveCharacterTextSplitter(chunk_size = chunk_size, chunk_overlap = chunk_overlap)
         if isinstance(t_data, str) == False:
